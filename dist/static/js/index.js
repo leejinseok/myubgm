4348e889fc31e04be16bd728ec1551a201239818
이진석 <sonaky47@gmail.com>
2018-05-29 00:00:27 +0900

    update

--- dist/static/js/index.js
+++ dist/static/js/index.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+  handleFullPage();
+  handleCarousel();
+});
+
+function handleFullPage () {
   var options = {
     menu: '#menu',
     anchors: ['about', 'randomMusic', 'category', 'recommandation'],
+    flankingItems: 5
   };
-	$('#fullpage').fullpage(options);
-});
+  $('#fullpage').fullpage(options);
+}
+
+var carousel;
+function handleCarousel () {
+  var option = {};
+  carousel = $("#carousel").waterwheelCarousel(option);
+}
+
+function carouselNext () {
+  carousel.next();
+}
+
+function carouselPrev () {
+  carousel.pev();
+}
